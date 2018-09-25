4196a23d84e90ccc50b4e3579a0ef7a8b18a1b4e
Zak <zakary.howard94@gmail.com>
2018-09-25 08:23:56 +0200

    added game engine to determine outcomes, added winner messages

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,3 +1,33 @@
-class RPS
 
+
+class RPS
+    attr_writer :rand_seed
+
+    def initialize(guess:)
+        @guess = guess.downcase
+    end
+
+    def computer_guess
+        srand (@rand_seed)
+        computer_guesses = %w{rock paper scissors}
+        computer_guesses.sample
+    end
+    
+    def winner_is
+        if rule_engine[computer_guess.to_sym].include? @guess
+            "Computer wins..."
+        elsif rule_engine[@guess.to_sym].include? computer_guess
+            "You win!"
+        else
+            "Draw"
+        end
+    end
+
+    def rule_engine
+        {
+            'rock': ['scissors'],
+            'paper': ['rock'],
+            'scissors': ['paper']
+        }
+    end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,3 +1,5 @@
+require './lib.rps'
+
 describe RPS do
 
     it "paper beats rock" do
